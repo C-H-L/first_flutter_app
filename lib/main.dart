0292a3a650381a74a4231d07831d62a1195b47a3
Heng Loong <>
2020-07-07 19:21:44 +0800

    Tut Part 2

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '1Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.orange,
+      ),
       home: RandomWords(),
     );
   }
@@ -25,6 +28,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
 
   @override
@@ -32,16 +36,50 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
+  void _pushSaved()
+  {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          final tiles = _saved.map((WordPair pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          });
+
+          final divided = ListTile.divideTiles(
+            context: context,
+              tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(title: Text('Saved Suggestions')),
+            body: ListView(children: divided),
+          );
+        },
+      )
+    );
+  }
+
   Widget _buildSuggestions() {
     return ListView.builder(
-      padding: EdgeInsets.all(16.0),
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 0,),
       itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
+        if (i.isOdd) return Divider(height: 1,);
 
         final index = i ~/ 2;
         if (index >= _suggestions.length) {
@@ -54,11 +92,25 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved)
+            _saved.remove(pair);
+          else
+            _saved.add(pair);
+        });
+      },
     );
   }
 }
